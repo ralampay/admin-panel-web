9dfaaa558ef0a453ec9b3841fcd02009cc8f6116
ralampay <raphael.alampay@gmail.com>
2024-08-11 14:46:11 +0800

    added menu clickable icon

--- src/js/TopNavigation.js
+++ src/js/TopNavigation.js
@@ -1,19 +1,34 @@
 import React from "react";
 import { destroySession } from "./services/AuthService";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default TopNavigation = () => {
 
   return (
-    <div>
-      <button
-        className="btn btn-danger"
-        onClick={() => {
-          destroySession();
-          window.location.href = "/";
-        }}
-      >
-        Logout
-      </button>
+    <div className="top-navigation">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col">
+            <FontAwesomeIcon 
+              icon={faBars}
+              size="2x"
+              className="clickable ms-2"
+            />
+          </div>
+          <div className="col text-end">
+            <button
+              className="btn btn-danger"
+              onClick={() => {
+                destroySession();
+                window.location.href = "/";
+              }}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
